21e651d4a5e07038bfe25f22b7938d1e018e95a0
Somiya <somiyapanikar@gmail.com>
2024-08-21 19:35:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/movieapp_volley_glide/Movie.java
@@ -0,0 +1,45 @@
+package com.example.movieapp_volley_glide;
+
+public class Movie {
+    private String title,poster,overview;
+    private Double rating;
+
+    public Movie(String title, String poster, String overview, Double rating) {
+        this.title = title;
+        this.poster = poster;
+        this.overview = overview;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+}
